7cca4edce641b979f95131b6b8e6a8f9767b12a0
Olstertecn11 <otzunund@miumg.edu.gt>
2022-12-06 20:21:17 -0600

    jfldjsfl

--- http_tcpServer_linux.cpp
+++ http_tcpServer_linux.cpp
@@ -3,6 +3,19 @@
 #include <sstream>
 #include <unistd.h>
 
+namespace{
+  const int BUFFER_SIZE = 30720;
+  
+  void log(const std::string &message){
+    std::cout << message << std::endl;
+  }
+
+  void exitWithError(const std::string &err){
+    log("ERROR: " + err);
+    exit(1);
+  }
+}
+
 
 namespace http{
   TcpServer::TcpServer(std::string ip_address, int port)
@@ -11,11 +24,14 @@ namespace http{
     m_socketAddress_len(sizeof(m_socketAddress)),
     m_serverMessage(buildResponse())
   {
-    this->startServer();
-
+    if(this->startServer() != 0){
+      std::ostringstream ss;
+      ss << "failed to start server with PORT: " << ntohs(m_socketAddress.sin_port);
+      log(ss.str());
+    }
   }
   TcpServer::~TcpServer(){
-
+    this->closeServer();
   }
   
   int TcpServer::startServer(){
